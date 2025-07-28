0e70fdfa8e8684c513e8bba2c61d52f1224d751d
khushi <khushipurohit171@gmail.com>
2025-07-28 23:24:03 +0530

    changes main

--- src/lc_practice/hill_valley.java
+++ src/lc_practice/hill_valley.java
@@ -1,21 +1,25 @@
 package lc_practice;
 
 public class hill_valley {
+    public static int countHillValley(int[] nums) {
+        int left = 0;
+        int count = 0;
 
-        public int countHillValley(int[] nums) {
-            int left = 0;
-            int count = 0;
-
-            for(int i = 1; i < nums.length - 1;i++){
-                if(nums[i] != nums[i+1]){
-                    if((nums[i] > nums[left] && nums[i] > nums[i+1]) || (nums[i] < nums[left] && nums[i] < nums[i+1])){
-                        count++;
-                    }
-                    left = i;
+        for(int i = 1; i < nums.length - 1;i++){
+            if(nums[i] != nums[i+1]){
+                if((nums[i] > nums[left] && nums[i] > nums[i+1]) || (nums[i] < nums[left] && nums[i] < nums[i+1])){
+                    count++;
+                }
+                left = i;
                 }
             }
+        return count;
+    }
 
-            return count;
-        }
+    public static void main(String[] args) {
+        int[] arr = {1,2,5,7,8,2,6};
+        int result = countHillValley(arr);
+        System.out.println(result);
+    }
 
 }
